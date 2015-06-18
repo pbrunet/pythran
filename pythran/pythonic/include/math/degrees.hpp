fc96d83d8b3bce11d69917e7e976eb280eec87f0
Pierrick Brunet <pierrick.brunet@telecom-bretagne.eu>
2015-06-18 09:05:20 +0200

    Fix includes for math module

--- pythran/pythonic/include/math/degrees.hpp
+++ pythran/pythonic/include/math/degrees.hpp
@@ -1,8 +1,8 @@
 #ifndef PYTHONIC_INCLUDE_MATH_DEGREES_HPP
 #define PYTHONIC_INCLUDE_MATH_DEGREES_HPP
 
-#include "pythonic/utils/proxy.hpp"
-#include "pythonic/math/pi.hpp"
+#include "pythonic/include/utils/proxy.hpp"
+#include "pythonic/include/math/pi.hpp"
 
 namespace pythonic
 {
